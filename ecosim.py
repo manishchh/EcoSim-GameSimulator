a2b9cf23ae329b7798977e68dfdeaa022ea928ce
Manish Chhetri <chhetrim901@gmail.com>
2021-08-31 23:32:55 +0930

    Created classes for wombat, snake & mongoose

--- ecosim.py
+++ ecosim.py
@@ -32,6 +32,7 @@ class Animal(GameObject):
         random.randint(self.get_y()-20, self.get_y()+20)) 
         self.speed = speed
         self.energy = energy
+        
     
     def update(self,timeElapsed):
         self.energy -= timeElapsed
@@ -42,6 +43,20 @@ class Animal(GameObject):
             trajectory = trajectory.normalize().scale(self.speed * timeElapsed)
             self.move_by(trajectory.x, trajectory.y)
 
+class Wombat(Animal):
+    def __init__(self, position, width, height , sourceImage, game,speed, energy):
+        super().__init__(position, width, height, sourceImage, game)
+
+class Snake(Animal):
+     def __init__(self, position, width, height , sourceImage, game,speed, energy):
+        super().__init__(position, width, height, sourceImage, game)
+
+class Mongoose(Animal):
+     def __init__(self, position, width, height , sourceImage, game,speed, energy):
+        super().__init__(position, width, height, sourceImage, game)
+
+
+
 class EcoSim(Game):
     def __init__(self):
         super().__init__()
@@ -50,14 +65,14 @@ class EcoSim(Game):
         # list_of_plants = [Grass, BlueBerryBush]
         # list_of_animals = [Wombat, Snake]
         for i in range(12):
-            for g in range(10):
+            for j in range(10):
                 random_list = random.choice(list_of_tiles)
                 if random_list == Dirt:
-                    tile = Dirt(Vector2D(i*64,g*64), 64, 64, ImageLibrary.get('dirt_tile'), self)
+                    tile = Dirt(Vector2D(i*64,j*64), 64, 64, ImageLibrary.get('dirt_tile'), self)
                     dirtCount+=1
                     
                 elif random_list == Sand:
-                    tile = Sand(Vector2D(i*64,g*64), 64, 64, ImageLibrary.get('sand_tile'), self)
+                    tile = Sand(Vector2D(i*64,j*64), 64, 64, ImageLibrary.get('sand_tile'), self)
         dirtCount = dirtCount/2
         for x in self.get_game_objs():
             if isinstance(x,Dirt) and dirtCount>0: 
