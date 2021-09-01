b94dd5c2029d31539450a48958fb827f09ed8dd5
Manish Chhetri <chhetrim901@gmail.com>
2021-09-01 22:59:29 +0930

    Generated a single womabt & tested eat grass flow

--- ecosim.py
+++ ecosim.py
@@ -32,7 +32,6 @@ class Animal(GameObject):
         random.randint(self.get_y()-20, self.get_y()+20)) 
         self.speed = speed
         self.energy = energy
-        
     
     def update(self,timeElapsed):
         self.energy -= timeElapsed
@@ -42,10 +41,49 @@ class Animal(GameObject):
             trajectory = self.target.subtract(self.get_position())
             trajectory = trajectory.normalize().scale(self.speed * timeElapsed)
             self.move_by(trajectory.x, trajectory.y)
+    
 
 class Wombat(Animal):
+    target= Vector2D(46,46)
     def __init__(self, position, width, height , sourceImage, game,speed, energy):
-        super().__init__(position, width, height, sourceImage, game)
+        super().__init__(position, width, height, sourceImage, game,speed,energy)
+        
+    def update(self,timeElapsed):
+
+        self.closestGrass = self.getClosestGrass()
+        self.target = self.closestGrass.get_position()
+        if self.checkIfTargetReached():
+            self.get_game()._remove_game_obj(self.closestGrass)
+            self.closestGrass = self.getClosestGrass()
+            self.target = self.closestGrass.get_position()
+        
+        trajectory = self.target.subtract(self.get_position())
+        trajectory = trajectory.normalize().scale(self.speed * timeElapsed)
+        self.move_by(trajectory.x, trajectory.y)
+
+    def checkIfTargetReached(self):
+        currentDistance = self.get_position().distance(self.target)
+        if(currentDistance<18):
+            return True
+        return False
+
+    
+    
+
+    def getClosestGrass(self):
+        gameObjects = self.get_game().get_game_objs()
+        closestGrass = gameObjects[0]
+        shortestDistance =  1000
+        for gameObject in gameObjects:
+            if isinstance(gameObject,Grass):
+                currentShortestDistance =  self.get_position().distance(gameObject.get_position())
+                if currentShortestDistance< shortestDistance:
+                    shortestDistance = currentShortestDistance
+                    closestGrass = gameObject
+        return closestGrass
+
+
+
 
 class Snake(Animal):
      def __init__(self, position, width, height , sourceImage, game,speed, energy):
@@ -55,7 +93,16 @@ class Mongoose(Animal):
      def __init__(self, position, width, height , sourceImage, game,speed, energy):
         super().__init__(position, width, height, sourceImage, game)
 
+class Plant(GameObject):
+    def __init__(self, position, width, height , sourceImage, game):
+        super().__init__(position, width, height, sourceImage, game)
 
+class Grass(Plant):
+    def __init__(self, position, width, height , sourceImage, game):
+        super().__init__(position, width, height, sourceImage, game)
+
+    def update(self, seconds):
+        return super().update(seconds)
 
 class EcoSim(Game):
     def __init__(self):
@@ -76,9 +123,10 @@ class EcoSim(Game):
         dirtCount = dirtCount/2
         for x in self.get_game_objs():
             if isinstance(x,Dirt) and dirtCount>0: 
-                Animal(x.get_position(),64, 64, ImageLibrary.get('wombat1'), self, 10, 3)
+                Grass(x.get_position(),64, 64, ImageLibrary.get('grass_tuft'), self)
                 dirtCount-=1
 
+        Wombat(Vector2D(480, 0),48,48,ImageLibrary.get('wombat1'),self,50,300)
 
         # self.animal = Animal(Vector2D(96, 96), 96, 96, ImageLibrary.get('wombat1'), self, 10, 3)
     
