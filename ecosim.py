8a93e80a710b268f4078185894c8bfe843f37520
Manish Chhetri <chhetrim901@gmail.com>
2021-09-05 23:50:27 +0930

    Refactored code and worked on wombat class

--- ecosim.py
+++ ecosim.py
@@ -1,146 +1,11 @@
+
+from animals.wombat import Wombat
+from tiles.dirtTile import Dirt
+from tiles.sandTile import Sand
+from plants.grass import Grass
+from plants.berrybush import BerryBush
 from game import *
 import random
-
-class Tile(GameObject):
-    def __init__(self, position, width, height, sourceImage, game):
-        super().__init__(position, width, height, sourceImage, game)
-
-
-    def update(self, timeElapsed):
-        print('Time Elapsed =', timeElapsed)
-
-# from tile import *
-
-class Dirt(Tile):
-
-    def __init__(self, position, width, height, sourceImage, game,plant):
-        super().__init__(position, width, height, sourceImage, game)
-        self.__plant = plant
-
-    def getPlant(self):
-        return self.__plant
-
-    def setPlant(self,plant):
-        self.__plant = plant
-    def update(self, timeElapsed):
-        #grow plants;
-        print("DirtTile")
-
-class Sand(Tile):
-    def __init__(self, position, width, height, sourceImage, game):
-        super().__init__(position, width, height, sourceImage, game)
-
-
-class Animal(GameObject):
-    def __init__(self, position, width, height , sourceImage, game,speed, energy):
-        super().__init__(position, width, height, sourceImage, game)
-        self.target = Vector2D(random.randint(self.get_x()-20, self.get_x()+20), 
-        random.randint(self.get_y()-20, self.get_y()+20)) 
-        self.speed = speed
-        self.energy = energy
-    
-    def update(self,timeElapsed):
-        self.energy -= timeElapsed
-        if self.energy <0:
-            self.destroy()
-        else:
-            trajectory = self.target.subtract(self.get_position())
-            trajectory = trajectory.normalize().scale(self.speed * timeElapsed)
-            self.move_by(trajectory.x, trajectory.y)
-    
-
-class Wombat(Animal):
-    target= Vector2D(46,46)
-    def __init__(self, position, width, height , sourceImage, game,speed, energy):
-        super().__init__(position, width, height, sourceImage, game,speed,energy)
-        
-    def update(self,timeElapsed):
-
-        self.closestGrass = self.getClosestGrass()
-        if(self.closestGrass == None):
-            print("grass finsidhed")
-        else:
-            self.target = self.closestGrass.get_position()
-            if self.checkIfTargetReached():
-                self.get_game()._remove_game_obj(self.closestGrass)
-                self.closestGrass = self.getClosestGrass()
-                if(self.closestGrass == None):
-                    return
-                else:  
-                    self.target = self.closestGrass.get_position()
-            
-            trajectory = self.target.subtract(self.get_position())
-            trajectory = trajectory.normalize().scale(self.speed * timeElapsed)
-            self.move_by(trajectory.x, trajectory.y)
-
-    def checkIfTargetReached(self):
-        currentDistance = self.get_position().distance(self.target)
-        if(currentDistance<18):
-            return True
-        return False
-
-    
-    def getClosestGrass(self):
-        gameObjects = self.get_game().get_game_objs()
-        closestGrass = gameObjects[0]
-        shortestDistance =  1000
-        grassExists = False
-        for gameObject in gameObjects:
-            if isinstance(gameObject,Grass):
-                grassExists =True
-                currentShortestDistance =  self.get_position().distance(gameObject.get_position())
-                if currentShortestDistance< shortestDistance:
-                    shortestDistance = currentShortestDistance
-                    closestGrass = gameObject
-        if grassExists!= True:
-            return None
-        return closestGrass
-
-
-
-
-class Snake(Animal):
-     def __init__(self, position, width, height , sourceImage, game,speed, energy):
-        super().__init__(position, width, height, sourceImage, game)
-
-class Mongoose(Animal):
-     def __init__(self, position, width, height , sourceImage, game,speed, energy):
-        super().__init__(position, width, height, sourceImage, game)
-
-class Plant(GameObject):
-    def __init__(self, position, width, height , sourceImage, game):
-        super().__init__(position, width, height, sourceImage, game)
-
-class Grass(Plant):
-    __lastUpdateTime =0
-    __grassRegenerationTime = 15
-    
-    def __init__(self, position, width, height , sourceImage, game):
-        super().__init__(position, width, height, sourceImage, game)
-
-    def update(self, seconds):
-        super().update(seconds)
-        self.__lastUpdateTime+=seconds
-        if(self.__lastUpdateTime >=self.__grassRegenerationTime):
-            self.generateNewGrass()
-            self.__lastUpdateTime = 0
-
-    def generateNewGrass(self):
-        dirtTiles = self.get_game().getDirtTiles()
-        for x in dirtTiles:
-            distance = Vector2D.distance(self.get_position(),x.get_position())
-            if distance == 64 and x.getPlant()==None:
-                grass = Grass(x.get_position(),64,64,ImageLibrary.get("grass_tuft"),self.get_game())
-                x.setPlant(grass)
-                break
-                
-           
-class BerryBush(Plant):
-    def __init__(self, position, width, height , sourceImage, game):
-        super().__init__(position, width, height, sourceImage, game)
-
-    def update(self, seconds):
-        return super().update(seconds)
 
 class EcoSim(Game):
     __listOfDirtTiles =[]
@@ -158,6 +23,7 @@ class EcoSim(Game):
                     tile = Sand(Vector2D(i*64,j*64), 64, 64, ImageLibrary.get('sand_tile'), self)
                     sandCount+=1
         self.generatePlants()
+        wombat = Wombat(Vector2D(128,128),64,64,ImageLibrary.get('wombat1'),self,30,100)
     
     def getDirtTiles(self):
         return self.__listOfDirtTiles
